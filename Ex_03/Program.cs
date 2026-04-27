952fce9beaca5ac12ca43a8ce3058677d12e4150
H-Akaogi <hazumi.akaogi@wemex.com>
2026-04-27 16:49:02 +0900

    start Ex03

---
+++ Ex_03/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Ex_03;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("数値を入力してください -> ");
+        Console.ReadLine();
+        
+    }
+}
