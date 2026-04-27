0c87a16a96022ea2d0fc42d15bcdf470ef7f948d
H-Akaogi <hazumi.akaogi@wemex.com>
2026-04-27 17:02:45 +0900

    finished Ex03

--- Ex_03/Program.cs
+++ Ex_03/Program.cs
@@ -4,8 +4,9 @@ static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("数値を入力してください -> ");
-        Console.ReadLine();
-        
+        System.Console.WriteLine("数値を入力してください -> ");
+        int num = int.Parse(Console.ReadLine());
+        System.Console.WriteLine($"5を加算した結果は {num+5} です");
+        System.Console.WriteLine($"5を減算した結果は {num-5} です");
     }
 }
