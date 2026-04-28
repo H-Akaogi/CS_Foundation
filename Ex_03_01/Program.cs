553842a6da98da3ed096c9aba13d71eaded4e38e
H-Akaogi <hazumi.akaogi@wemex.com>
2026-04-28 15:06:58 +0900

    finished Ex_03_01

--- Ex_03_01/Program.cs
+++ Ex_03_01/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace Ex_03_01;
+﻿using System.Net.ServerSentEvents;
+
+namespace Ex_03_01;
 
 static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // インスタンスの生成
+        Item item = new Item();
+        // フィールドに値を設定する
+        Console.Write("商品番号を入力してください -> ");
+        item.itemNo = int.Parse(Console.ReadLine());
+        Console.Write("商品名を入力してください -> ");
+        item.itemName = Console.ReadLine();
+        Console.Write("単価を入力してください -> ");
+        item.price = int.Parse(Console.ReadLine());
+        // フィールドの値を取得する
+        Console.WriteLine("結果");
+        Console.WriteLine($"商品番号 = {item.itemNo}");
+        Console.WriteLine($"商品名 = {item.itemName}");
+        Console.WriteLine($"単価 = {item.price}");
     }
 }
