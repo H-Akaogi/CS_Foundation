a014d54a264b7ebde9ec78874f083d45d6cd4f83
H-Akaogi <hazumi.akaogi@wemex.com>
2026-04-28 14:55:28 +0900

    Itemクラスの定義

--- Ex_03_01/Item.cs
+++ Ex_03_01/Item.cs
@@ -4,5 +4,7 @@ namespace Ex_03_01; //namespaceは所属場所（Programと揃える）
 /// </summary>
 public class Item //ファイル名と揃える
 {
-
+    public int itemNo;      //商品番号
+    public string itemName; //商品名
+    public int price;	    //単価
 }
