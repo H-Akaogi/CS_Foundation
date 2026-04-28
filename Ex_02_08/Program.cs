49ed5b24900b1e05c54a083c3db0844ebdd53823
H-Akaogi <hazumi.akaogi@wemex.com>
2026-04-28 12:32:21 +0900

    finished Ex_02_08

---
+++ Ex_02_08/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Ex_02_08;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        string[] inputStation = { "品川", "大崎", "五反田", "目黒", "恵比寿", "渋谷", "原宿", "代々木", "新宿" };
+
+        //var station = inputStation[0];
+        var i = 1;
+        while (i < inputStation.Length)
+        {
+            Console.WriteLine($"{inputStation[i]}");
+            i++;
+        }
+
+    }
+}
