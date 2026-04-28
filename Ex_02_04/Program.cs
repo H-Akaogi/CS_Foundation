522019e81da2bc0981a7a54832fa205fbe6a254b
H-Akaogi <hazumi.akaogi@wemex.com>
2026-04-28 10:11:16 +0900

    finished Ex02_04

---
+++ Ex_02_04/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex_02_04;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("数値1を入力してください -> ");
+        int a = int.Parse(System.Console.ReadLine());
+        System.Console.Write("数値2を入力してください -> ");
+        int b = int.Parse(System.Console.ReadLine());
+        System.Console.Write("数値3を入力してください -> ");
+        int c = int.Parse(System.Console.ReadLine());
+
+        System.Console.WriteLine("入力結果");
+        System.Console.WriteLine($"数値1:{a}");
+        System.Console.WriteLine($"数値2:{b}");
+        System.Console.WriteLine($"数値3:{c}");
+    }
+}
