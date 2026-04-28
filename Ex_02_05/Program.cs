2d3c31787cfeb8f7379fb88d26c74bf2db9f6c87
H-Akaogi <hazumi.akaogi@wemex.com>
2026-04-28 11:27:14 +0900

    updated Ex_02_05

--- Ex_02_05/Program.cs
+++ Ex_02_05/Program.cs
@@ -7,13 +7,23 @@ static class Program
         Console.Write("値を入力してください -> ");
         int value = int.Parse(Console.ReadLine());
 
-        if (value%2 == 1)
+        if (value % 2 == 1)
         {
             Console.WriteLine($"結果：{value}");
         }
         else
         {
-            Console.WriteLine($"結果：{value*10}");
+            Console.WriteLine($"結果：{value * 10}");
         }
+        /*より簡単
+        Console.Write("値を入力してください -> ");
+        int value = int.Parse(Console.ReadLine());
+
+        if (value%2 == 0)
+        {
+           value *= 10
+        }
+        Console.WriteLine($"結果：{value}");
+        */
     }
 }
