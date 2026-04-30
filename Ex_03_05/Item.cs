c19a3e2ac183f2e7099db00ad688f09947f98097
H-Akaogi <hazumi.akaogi@wemex.com>
2026-04-30 11:20:27 +0900

    finished Ex_03_05

---
+++ Ex_03_05/Item.cs
@@ -0,0 +1,37 @@
+namespace Ex_03_05; //namespaceは所属場所（Programと揃える）
+/// <summary>
+/// 商品情報を表すクラス
+/// </summary>
+public class Item //ファイル名と揃える。クラスの中身は{}でまとめる
+{
+    //  フィールド一覧
+    public int itemNo;      //商品番号(フィールド)
+    public string itemName; //商品名(フィールド)
+    public int price;       //単価(フィールド)
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="pitemNo"></param>
+    /// <param name="pitemName"></param>
+    /// <param name="pprice"></param>
+    public Item(int pitemNo, string pitemName, int pprice)
+    //引数でフィールドを初期化する。変数名(pitemNoなど）はmainと対応していなくてOK
+    {
+        this.itemNo = pitemNo;
+        this.itemName = pitemName;
+        this.price = pprice;
+    }
+
+    /// <summary>
+    /// Printメソッド
+    /// </summary>
+    public void Print()         //itemNo、itemName、priceフィールドの値を出力する。
+    {
+        Console.WriteLine("結果");
+        Console.WriteLine($"商品番号 = {this.itemNo}");
+        Console.WriteLine($"商品名 = {this.itemName}");
+        Console.WriteLine($"単価 = {this.price}");
+    }
+}
+
--- Ex_03_05/Program.cs
+++ Ex_03_05/Program.cs
@@ -4,6 +4,17 @@ static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        ///型数・変数名を設定
+        Console.Write("商品番号を入力してください -> ");
+        int itemNo = int.Parse(Console.ReadLine()); //ドットの前にインスタンスを付けて呼び出す
+        Console.Write("商品名を入力してください -> ");
+        string itemName = Console.ReadLine();         //Console.Readは一文字のみ読み取る。
+        Console.Write("単価を入力してください -> ");
+        int price = int.Parse(Console.ReadLine());
+
+        ///itemの引数の初期値がないため、後ろに設定する
+        Item item = new Item(itemNo, itemName, price);
+        item.Print();
     }
 }
+
