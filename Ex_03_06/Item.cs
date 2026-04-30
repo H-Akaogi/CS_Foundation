304109d12d9e6d6b1093703c747e85e3366c4623
H-Akaogi <hazumi.akaogi@wemex.com>
2026-04-30 13:15:05 +0900

    finished Ex_03_06

---
+++ Ex_03_06/Item.cs
@@ -0,0 +1,45 @@
+namespace Ex_03_06; //namespaceは所属場所（Programと揃える）
+/// <summary>
+/// 商品情報を表すクラス
+/// </summary>
+public class Item //ファイル名と揃える。クラスの中身は{}でまとめる
+{
+    //  フィールド一覧
+    public int itemNo;      //商品番号(フィールド)
+    public string itemName; //商品名(フィールド)
+    public int price;       //単価(フィールド)
+    public double salesTaxRate = 0.10; // 消費税率(10%)
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public Item()
+    {
+    }
+    public Item(int pitemNo, string pitemName, int pprice)
+    {
+        this.itemNo = pitemNo;
+        this.itemName = pitemName;
+        this.price = pprice;
+    }
+    public void Print()         //itemNo、itemName、priceフィールドの値を出力する。
+    {
+        Console.WriteLine($"商品番号 = {this.itemNo}");
+        Console.WriteLine($"商品名 = {this.itemName}");
+        Console.WriteLine($"単価 = {this.price}");
+    }
+    public void ChangePrice(int pprice)   //priceフィールドの値を引数の値に変更する
+    {
+        price = pprice;         //priceをppriceに上書き（priceにppriceを代入）
+    }
+    public void ChangePrice(string pprice)   //priceフィールドの値を引数の値に変更する
+    {
+        price = int.Parse(pprice);         //priceをppriceに上書き（priceにppriceを代入）
+    }
+    public int CalculateTaxAmount()
+    {
+        double result = price + (price * salesTaxRate);
+        return (int)result;
+    }
+
+}
---
+++ Ex_03_06/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ex_03_06;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        ///型数・変数名を設定
+        Item item = new Item();
+        Console.Write("商品番号を入力してください -> ");
+        item.itemNo = int.Parse(Console.ReadLine()); //ドットの前にインスタンスを付けて呼び出す
+        Console.Write("商品名を入力してください -> ");
+        item.itemName = Console.ReadLine();         //Console.Readは一文字のみ読み取る。
+        Console.Write("単価を入力してください -> ");
+        item.price = int.Parse(Console.ReadLine());
+        Console.Write("新しい単価を入力してください -> ");
+        item.ChangePrice(int.Parse(Console.ReadLine()));
+        Console.WriteLine("結果");
+        item.Print();
+
+        Console.Write("更に新しい単価を入力してください -> ");
+        item.ChangePrice(Console.ReadLine());
+        Console.WriteLine("結果");
+        item.Print();
+
+    }
+}
