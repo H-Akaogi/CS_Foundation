1ca5987ca6792cecc639a1f8a4659cdb6fdbcedc
H-Akaogi <hazumi.akaogi@wemex.com>
2026-05-01 14:17:28 +0900

    finigshed Ex_03_11

---
+++ Ex_03_13/Circle.cs
@@ -0,0 +1,22 @@
+namespace Ex_03_13;
+
+public class Circle
+{
+    ///フィールド
+    public static double PI = 3.14; //円周率
+    int radius; //半径
+    ///メソッド
+    public double AreaCalculation()//フィールドの半径の値の円の面積を計算する動的メソッド
+    {
+        return radius * radius * PI;
+    }
+    public static double AreaCalculation(int radius) //引数の半径の値の円の面積を計算する静的メソッド
+    {
+        return radius * radius * PI;
+    }
+    ///コンストラクタ
+    public Circle(int pradius)
+    {
+        this.radius = pradius;//フィールドの半径を初期化するコンストラクタ
+    }
+}
